b3879605b155540c4b7795e0953530ec92331027
ruama <vanessaruama@gmail.com>
2024-07-24 14:24:13 -0300

    utilizar firebase

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,18 @@
-// src/environments/environment.prod.ts
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 export const environment = {
-  production: true,
-  apiUrl: 'https://dogshlter.vercel.app/api' // URL do backend para produção
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBNU16EKdMNUBlj3kzIZ4JXVSAyiBCG-gY",
+    authDomain: "dog-shelter-a8e65.firebaseapp.com",
+    projectId: "dog-shelter-a8e65",
+    storageBucket: "dog-shelter-a8e65.appspot.com",
+    messagingSenderId: "612716648277",
+    appId: "1:612716648277:web:1f88a715cd5ee95a6041dc",
+    measurementId: "G-CF3FXWS7MK"
+  }
 };
+
+const app = initializeApp(environment.firebaseConfig);
+const analytics = getAnalytics(app);
